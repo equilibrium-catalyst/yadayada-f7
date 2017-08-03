1c6d92fe1b8970bd9fcc0f15629b0c3a481e7edb
Thomas Wang <dev@paced.me>
2017-08-03 18:18:12 +1000

    Copied over bootstrapped files

---
+++ ctf/manage.py
@@ -0,0 +1,34 @@
+"""Launches and renders routes for paced's resume page."""
+
+import os
+
+from flask import (Flask, make_response, redirect, render_template, request,
+                   send_from_directory)
+
+# Necessary settings
+DEBUG = True
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    """Standard page redirect."""
+    return redirect("/level1/", code=301)
+
+
+@app.route('/sitemap.xml', methods=['GET'])
+def sitemap():
+    """Contact forms."""
+    return send_from_directory('static/', 'sitemap.xml')
+
+
+@app.route('/robots.txt')
+def robots():
+    """Robot file for SEO."""
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'robots.txt')
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG, host='localhost')
